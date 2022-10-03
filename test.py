b18eb514beb94b2cd34c3fd7576d2d1f80d1dbb8
Max Hager <maxhager28@gmail.com>
2022-10-03 20:28:16 +0200

    s

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
 import config
-subprocess.call(['git', 'add', '-A'])
+subprocess.call(['git', 'add', '/Users/maxhager/Projects2022/CronjobManager/test.log' '/Users/maxhager/Projects2022/CronjobManager/README.md'], cwd="/Users/maxhager/Projects2022/CronjobManager")
 subprocess.call(['git', 'commit', '-m', '{}'.format("Update")])
 subprocess.call(['git', 'push', 'https://{}@github.com/yachty66/CronjobManager.git'.format(config.gitToken)])
